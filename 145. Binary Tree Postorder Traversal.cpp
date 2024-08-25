8d56578abedff53387a7cdd676007d1a39dad15d
Sumant Yadav <116252911+Sumant3086@users.noreply.github.com>
2024-08-25 13:30:02 +0530

    Add files via upload

--- 145. Binary Tree Postorder Traversal.cpp
+++ 145. Binary Tree Postorder Traversal.cpp
@@ -41,20 +41,58 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// /**
+//  * Definition for a binary tree node.
+//  * struct TreeNode {
+//  *     int val;
+//  *     TreeNode *left;
+//  *     TreeNode *right;
+//  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+//  * };
+//  */
+// class Solution {
+// public:
+// void helper(TreeNode* root,vector<int>& v){
+//     if(root==nullptr){
+//         return;
+//     }
+//     helper(root->left,v);
+//     helper(root->right,v);
+//     v.push_back(root->val);
+// }
+//     vector<int> postorderTraversal(TreeNode* root) {
+//         vector<int>v;
+//         helper(root,v);
+//         return v;
+//     }
+// };
+
 class Solution {
 public:
-void helper(TreeNode* root,vector<int>&v){
-    if(!root){
-        return;
-    }
-    helper(root->left,v);
-    helper(root->right,v);
-    v.push_back(root->val);
-    
-}
     vector<int> postorderTraversal(TreeNode* root) {
-        vector<int>v;
-        helper(root,v);
-        return v;
+        vector<int> ans;
+        while (root) {
+            if (!root->right) {
+                ans.push_back(root->val);
+                root = root->left;
+            } else {
+                TreeNode* next = root->right;
+                while (next->left && next->left != root) {
+                    next = next->left;
+                }
+                if (!next->left) {
+                    ans.push_back(root->val);
+                    next->left = root;
+                    root = root->right;
+                } else {
+                    next->left = nullptr;
+                    root = root->left;
+                }
+            }
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
     }
 };
