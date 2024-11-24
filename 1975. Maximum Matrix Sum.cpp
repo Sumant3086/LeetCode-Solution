6a917426cdffde7c34b3ff9381f4d58e7dfb0149
Sumant <sumantyadav3086@gmail.com>
2024-11-24 22:19:38 +0530

    updated

--- 1975. Maximum Matrix Sum.cpp
+++ 1975. Maximum Matrix Sum.cpp
@@ -56,23 +56,25 @@
 
 
 // Copyright ©️ 2024 LeetCode All rights reserved
-
 class Solution {
-    public long maxMatrixSum(int[][] matrix) {
-    int min = Integer.MAX_VALUE;
-    long sum = 0;
-    int negCount = 0; 
-    for(int i=0; i<matrix.length; i++)
-    for(int j=0; j<matrix[0].length; j++)
-    {
-     if(matrix[i][j]<0)
-     negCount++;
-     int ab = Math.abs(matrix[i][j]);
-     min = Math.min(min, ab);
-     sum += ab;    
+public:
+    long long maxMatrixSum(vector<vector<int>>& matrix) {
+        int minValue = INT_MAX;
+        long long sum = 0;
+        int negCount = 0;
+
+        for (int i = 0; i < matrix.size(); i++) {
+            for (int j = 0; j < matrix[0].size(); j++) {
+                if (matrix[i][j] < 0)
+                    negCount++;
+                int absValue = abs(matrix[i][j]);
+                minValue = min(minValue, absValue);
+                sum += absValue;
+            }
+        }
+
+        if (negCount % 2 == 0)
+            return sum;
+        return sum - 2 * minValue;
     }
-    if(negCount%2==0)
-    return sum; 
-    return sum - 2*min;
-}
-}
+};
