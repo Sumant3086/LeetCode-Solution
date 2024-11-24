47daf0f44e8b84165d6c855fb384615111d29f22
Sumant <sumantyadav3086@gmail.com>
2024-11-24 22:18:23 +0530

    maxMatrix

---
+++ 1975. Maximum Matrix Sum.cpp
@@ -0,0 +1,78 @@
+// 1975. Maximum Matrix Sum
+// Medium
+// Topics
+// Companies
+// Hint
+// You are given an n x n integer matrix. You can do the following operation any number of times:
+
+// Choose any two adjacent elements of matrix and multiply each of them by -1.
+// Two elements are considered adjacent if and only if they share a border.
+
+// Your goal is to maximize the summation of the matrix's elements. Return the maximum sum of the matrix's elements using the operation mentioned above.
+
+ 
+
+// Example 1:
+
+
+// Input: matrix = [[1,-1],[-1,1]]
+// Output: 4
+// Explanation: We can follow the following steps to reach sum equals 4:
+// - Multiply the 2 elements in the first row by -1.
+// - Multiply the 2 elements in the first column by -1.
+// Example 2:
+
+
+// Input: matrix = [[1,2,3],[-1,-2,-3],[1,2,3]]
+// Output: 16
+// Explanation: We can follow the following step to reach sum equals 16:
+// - Multiply the 2 last elements in the second row by -1.
+ 
+
+// Constraints:
+
+// n == matrix.length == matrix[i].length
+// 2 <= n <= 250
+// -105 <= matrix[i][j] <= 105
+
+// Seen this question in a real interview before?
+// 1/5
+// Yes
+// No
+// Accepted
+// 76.9K
+// Submissions
+// 120K
+// Acceptance Rate
+// 64.1%
+// Topics
+// Companies
+// Hint 1
+// Hint 2
+// Discussion (149)
+
+// Choose a type
+
+
+
+// Copyright ©️ 2024 LeetCode All rights reserved
+
+class Solution {
+    public long maxMatrixSum(int[][] matrix) {
+    int min = Integer.MAX_VALUE;
+    long sum = 0;
+    int negCount = 0; 
+    for(int i=0; i<matrix.length; i++)
+    for(int j=0; j<matrix[0].length; j++)
+    {
+     if(matrix[i][j]<0)
+     negCount++;
+     int ab = Math.abs(matrix[i][j]);
+     min = Math.min(min, ab);
+     sum += ab;    
+    }
+    if(negCount%2==0)
+    return sum; 
+    return sum - 2*min;
+}
+}
