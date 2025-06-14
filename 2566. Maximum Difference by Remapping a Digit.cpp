8fc320caff627ec86b4ed7b91dad61137a237ff7
Sumant <sumantyadav3086@gmail.com>
2025-06-14 22:13:01 +0530

    maxi ans mini

---
+++ 2566. Maximum Difference by Remapping a Digit.cpp
@@ -0,0 +1,87 @@
+// 2566. Maximum Difference by Remapping a Digit
+// Easy
+// Topics
+// premium lock icon
+// Companies
+// Hint
+// You are given an integer num. You know that Bob will sneakily remap one of the 10 possible digits (0 to 9) to another digit.
+
+// Return the difference between the maximum and minimum values Bob can make by remapping exactly one digit in num.
+
+// Notes:
+
+// When Bob remaps a digit d1 to another digit d2, Bob replaces all occurrences of d1 in num with d2.
+// Bob can remap a digit to itself, in which case num does not change.
+// Bob can remap different digits for obtaining minimum and maximum values respectively.
+// The resulting number after remapping can contain leading zeroes.
+ 
+
+// Example 1:
+
+// Input: num = 11891
+// Output: 99009
+// Explanation: 
+// To achieve the maximum value, Bob can remap the digit 1 to the digit 9 to yield 99899.
+// To achieve the minimum value, Bob can remap the digit 1 to the digit 0, yielding 890.
+// The difference between these two numbers is 99009.
+// Example 2:
+
+// Input: num = 90
+// Output: 99
+// Explanation:
+// The maximum value that can be returned by the function is 99 (if 0 is replaced by 9) and the minimum value that can be returned by the function is 0 (if 9 is replaced by 0).
+// Thus, we return 99.
+ 
+
+// Constraints:
+
+// 1 <= num <= 108
+
+// Seen this question in a real interview before?
+// 1/5
+// Yes
+// No
+// Accepted
+// 93,279/124.8K
+// Acceptance Rate
+// 74.8%
+// Topics
+// icon
+// Companies
+// Hint 1
+// Hint 2
+// Discussion (157)
+
+// Choose a type
+
+
+
+// Copyright © 2025 LeetCode. All rights reserved.
+
+// 489
+
+
+// 157
+
+
+class Solution {
+public:
+    int minMaxDifference(int num) {
+        string s=to_string(num);
+        string maxi=s;
+        string mini=s;
+        for(char c:s){
+            if(c!='9'){
+                replace(maxi.begin(),maxi.end(),c,'9');
+                break;
+            }
+        }
+        for(char c:s){
+            if(c!='0'){
+                replace(mini.begin(),mini.end(),c,'0');
+                break;
+            }
+        }
+        return stoi(maxi)-stoi(mini);
+    }
+};
