261be1bc8742d0e96676a52e32f231499fe31ffb
sumant3086 <sumantyadav3086@gmail.com>
2025-07-17 23:54:34 +0530

    brute force and dp solution

---
+++ 3202. Find the Maximum Length of Valid Subsequence II.cpp
@@ -0,0 +1,116 @@
+// 3202. Find the Maximum Length of Valid Subsequence II
+// Solved
+// Medium
+// Topics
+// premium lock icon
+// Companies
+// Hint
+// You are given an integer array nums and a positive integer k.
+// A subsequence sub of nums with length x is called valid if it satisfies:
+
+// (sub[0] + sub[1]) % k == (sub[1] + sub[2]) % k == ... == (sub[x - 2] + sub[x - 1]) % k.
+// Return the length of the longest valid subsequence of nums.
+ 
+
+// Example 1:
+
+// Input: nums = [1,2,3,4,5], k = 2
+
+// Output: 5
+
+// Explanation:
+
+// The longest valid subsequence is [1, 2, 3, 4, 5].
+
+// Example 2:
+
+// Input: nums = [1,4,2,3,1,4], k = 3
+
+// Output: 4
+
+// Explanation:
+
+// The longest valid subsequence is [1, 4, 1, 4].
+
+ 
+
+// Constraints:
+
+// 2 <= nums.length <= 103
+// 1 <= nums[i] <= 107
+// 1 <= k <= 103
+
+// Seen this question in a real interview before?
+// 1/5
+// Yes
+// No
+// Accepted
+// 79,392/141.9K
+// Acceptance Rate
+// 55.9%
+// Topics
+// icon
+// Companies
+// Hint 1
+// Hint 2
+// Hint 3
+// Similar Questions
+// Discussion (120)
+
+// Choose a type
+
+
+
+// Copyright © 2025 LeetCode. All rights reserved.
+
+
+class Solution {
+public:
+    int maximumLength(vector<int>& nums, int k) {
+        int n=nums.size();
+        int res=1;
+        vector<unordered_map<int,int>>dp(n);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                // cout<<nums[j]<<" "<<nums[i]<<" ";
+                int val=(nums[i]+nums[j])%k;
+                int len=2;
+                if(dp[j].count(val)){
+                    len=dp[j][val]+1;
+                }
+                dp[i][val]=max(dp[i][val],len);
+                res=max(res,dp[i][val]);
+            }
+            // cout<<endl;
+        }
+        return res;
+    }
+};
+// class Solution {
+// public:
+//     int maximumLength(vector<int>& nums, int k) {
+//         int n=nums.size();
+//         int maxL=0;
+//         for(int i=0;i<(1<<n);i++){
+//             vector<int>arr;
+//             for(int j=0;j<n;j++){
+//                 if(i & (1<<j)){
+//                     arr.push_back(nums[j]);
+//                 }
+//             }
+//             if(arr.size()<2)continue;
+//             int val=(arr[0]+arr[1])%k;
+//             bool valid=true;
+//             for(int i=1;i<arr.size();i++){
+//                 if((arr[i-1]+arr[i])%k!=val){
+//                     valid=false;
+//                     break;
+//                 }
+//             }
+//             if(valid){
+//                 maxL=max(maxL,(int)arr.size());
+//             }
+//         }
+//         return maxL;
+//     }
+// };
