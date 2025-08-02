6c7fa3167028a75a52ae4a173c31a50fc07c1f7f
sumant3086 <sumantyadav3086@gmail.com>
2025-08-02 23:46:03 +0530

    store both extra value in diff vector

---
+++ 2561. Rearranging Fruits.cpp
@@ -0,0 +1,582 @@
+// 2561. Rearranging Fruits
+// Solved
+// Hard
+// Topics
+// premium lock icon
+// Companies
+// Hint
+// You have two fruit baskets containing n fruits each. You are given two 0-indexed integer arrays basket1 and basket2 representing the cost of fruit in each basket. You want to make both baskets equal. To do so, you can use the following operation as many times as you want:
+
+// Chose two indices i and j, and swap the ith fruit of basket1 with the jth fruit of basket2.
+// The cost of the swap is min(basket1[i],basket2[j]).
+// Two baskets are considered equal if sorting them according to the fruit cost makes them exactly the same baskets.
+
+// Return the minimum cost to make both the baskets equal or -1 if impossible.
+
+ 
+
+// Example 1:
+
+// Input: basket1 = [4,2,2,2], basket2 = [1,4,1,2]
+// Output: 1
+// Explanation: Swap index 1 of basket1 with index 0 of basket2, which has cost 1. Now basket1 = [4,1,2,2] and basket2 = [2,4,1,2]. Rearranging both the arrays makes them equal.
+// Example 2:
+
+// Input: basket1 = [2,3,4,1], basket2 = [3,2,5,1]
+// Output: -1
+// Explanation: It can be shown that it is impossible to make both the baskets equal.
+ 
+
+// Constraints:
+
+// basket1.length == basket2.length
+// 1 <= basket1.length <= 105
+// 1 <= basket1[i],basket2[i] <= 109
+// Seen this question in a real interview before?
+// 1/5
+// Yes
+// No
+// Accepted
+// 67,965/121.1K
+// Acceptance Rate
+// 56.1%
+// Topics
+// icon
+// Companies
+// Hint 1
+// Hint 2
+// Hint 3
+// Hint 4
+// Hint 5
+// Similar Questions
+// Discussion (123)
+
+// Choose a type
+
+
+
+// Copyright © 2025 LeetCode. All rights reserved.
+
+// 784
+
+
+// 123
+
+
+// 6911 Online
+// C++
+// Auto
+
+
+
+
+
+// 23242526272221
+//         long long res=0;
+//         for(int i=0;i<extra1.size();i++){
+//             res+=min((long long)min(extra1[i],extra2[i]),2LL*mini);
+//         } 
+//         return res;
+//     }
+// };
+// i.first
+// No results
+// Saved
+// Accepted
+// Runtime: 0 ms
+// Case 1
+// Case 2
+// Case 3
+// Input
+// basket1 =
+// [4,2,2,2]
+// basket2 =
+// [1,4,1,2]
+// Output
+// 1
+// Expected
+// 1
+// Contribute a testcase
+// Daily Question
+
+// Tag
+// 2965. Find Missing and Repeated Values
+// Easy
+
+// 2523. Closest Prime Numbers in Range
+// Med.
+
+// 2379. Minimum Recolors to Get K Consecutive Black Blocks
+// Easy
+
+// 3208. Alternating Groups II
+// Med.
+
+// 3306. Count of Substrings Containing Every Vowel and K Consonants II
+// Med.
+
+// 1358. Number of Substrings Containing All Three Characters
+// Med.
+
+// 2529. Maximum Count of Positive Integer and Negative Integer
+// Easy
+
+// 3356. Zero Array Transformation II
+// Med.
+
+// 2226. Maximum Candies Allocated to K Children
+// Med.
+
+// 2560. House Robber IV
+// Med.
+
+// 2594. Minimum Time to Repair Cars
+// Med.
+
+// 2206. Divide Array Into Equal Pairs
+// Easy
+
+// 2401. Longest Nice Subarray
+// Med.
+
+// 3191. Minimum Operations to Make Binary Array Elements Equal to One I
+// Med.
+
+// 3108. Minimum Cost Walk in Weighted Graph
+// Hard
+
+// 2115. Find All Possible Recipes from Given Supplies
+// Med.
+
+// 2685. Count the Number of Complete Components
+// Med.
+
+// 1976. Number of Ways to Arrive at Destination
+// Med.
+
+// 3169. Count Days Without Meetings
+// Med.
+
+// 3394. Check if Grid can be Cut into Sections
+// Med.
+
+// 2033. Minimum Operations to Make a Uni-Value Grid
+// Med.
+
+// 2780. Minimum Index of a Valid Split
+// Med.
+
+// 2503. Maximum Number of Points From Grid Queries
+// Hard
+
+// 2818. Apply Operations to Maximize Score
+// Hard
+
+// 763. Partition Labels
+// Med.
+
+// 2551. Put Marbles in Bags
+// Hard
+
+// 2140. Solving Questions With Brainpower
+// Med.
+
+// 2873. Maximum Value of an Ordered Triplet I
+// Easy
+
+// 2874. Maximum Value of an Ordered Triplet II
+// Med.
+
+// 1123. Lowest Common Ancestor of Deepest Leaves
+// Med.
+
+// 1863. Sum of All Subset XOR Totals
+// Easy
+
+// 368. Largest Divisible Subset
+// Med.
+
+// 416. Partition Equal Subset Sum
+// Med.
+
+// 3396. Minimum Number of Operations to Make Elements in Array Distinct
+// Easy
+
+// 3375. Minimum Operations to Make Array Values Equal to K
+// Easy
+
+// 2999. Count the Number of Powerful Integers
+// Hard
+
+// 2843. Count Symmetric Integers
+// Easy
+
+// 3272. Find the Count of Good Integers
+// Hard
+
+// 1922. Count Good Numbers
+// Med.
+
+// 1534. Count Good Triplets
+// Easy
+
+// 2179. Count Good Triplets in an Array
+// Hard
+
+// 2537. Count the Number of Good Subarrays
+// Med.
+
+// 2176. Count Equal and Divisible Pairs in an Array
+// Easy
+
+// 38. Count and Say
+// Med.
+
+// 2563. Count the Number of Fair Pairs
+// Med.
+
+// 781. Rabbits in Forest
+// Med.
+
+// 2145. Count the Hidden Sequences
+// Med.
+
+// 2338. Count the Number of Ideal Arrays
+// Hard
+
+// 1399. Count Largest Group
+// Easy
+
+// 2799. Count Complete Subarrays in an Array
+// Med.
+
+// 2845. Count of Interesting Subarrays
+// Med.
+
+// 2444. Count Subarrays With Fixed Bounds
+// Hard
+
+// 3392. Count Subarrays of Length Three With a Condition
+// Easy
+
+// 2302. Count Subarrays With Score Less Than K
+// Hard
+
+// 2962. Count Subarrays Where Max Element Appears at Least K Times
+// Med.
+
+// 1295. Find Numbers with Even Number of Digits
+// Easy
+
+// 2071. Maximum Number of Tasks You Can Assign
+// Hard
+
+// 838. Push Dominoes
+// Med.
+
+// 1007. Minimum Domino Rotations For Equal Row
+// Med.
+
+// 1128. Number of Equivalent Domino Pairs
+// Easy
+
+// 790. Domino and Tromino Tiling
+// Med.
+
+// 1920. Build Array from Permutation
+// Easy
+
+// 3341. Find Minimum Time to Reach Last Room I
+// Med.
+
+// 3342. Find Minimum Time to Reach Last Room II
+// Med.
+
+// 3343. Count Number of Balanced Permutations
+// Hard
+
+// 2918. Minimum Equal Sum of Two Arrays After Replacing Zeros
+// Med.
+
+// 1550. Three Consecutive Odds
+// Easy
+
+// 2094. Finding 3-Digit Even Numbers
+// Easy
+
+// 3335. Total Characters in String After Transformations I
+// Med.
+
+// 3337. Total Characters in String After Transformations II
+// Hard
+
+// 2900. Longest Unequal Adjacent Groups Subsequence I
+// Easy
+
+// 2901. Longest Unequal Adjacent Groups Subsequence II
+// Med.
+
+// 75. Sort Colors
+// Med.
+
+// 1931. Painting a Grid With Three Different Colors
+// Hard
+
+// 3024. Type of Triangle
+// Easy
+
+// 3355. Zero Array Transformation I
+// Med.
+
+// 73. Set Matrix Zeroes
+// Med.
+
+// 3362. Zero Array Transformation III
+// Med.
+
+// 3068. Find the Maximum Sum of Node Values
+// Hard
+
+// 2942. Find Words Containing Character
+// Easy
+
+// 2131. Longest Palindrome by Concatenating Two Letter Words
+// Med.
+
+// 1857. Largest Color Value in a Directed Graph
+// Hard
+
+// 2894. Divisible and Non-divisible Sums Difference
+// Easy
+
+// 3372. Maximize the Number of Target Nodes After Connecting Trees I
+// Med.
+
+// 3373. Maximize the Number of Target Nodes After Connecting Trees II
+// Hard
+
+// 2359. Find Closest Node to Given Two Nodes
+// Med.
+
+// 909. Snakes and Ladders
+// Med.
+
+// 2929. Distribute Candies Among Children II
+// Med.
+
+// 135. Candy
+// Hard
+
+// 1298. Maximum Candies You Can Get from Boxes
+// Hard
+
+// 3403. Find the Lexicographically Largest String From the Box I
+// Med.
+
+// 1061. Lexicographically Smallest Equivalent String
+// Med.
+
+// 2434. Using a Robot to Print the Lexicographically Smallest String
+// Med.
+
+// 3170. Lexicographically Minimum String After Removing Stars
+// Med.
+
+// 386. Lexicographical Numbers
+// Med.
+
+// 440. K-th Smallest in Lexicographical Order
+// Hard
+
+// 3442. Maximum Difference Between Even and Odd Frequency I
+// Easy
+
+// 3445. Maximum Difference Between Even and Odd Frequency II
+// Hard
+
+// 3423. Maximum Difference Between Adjacent Elements in a Circular Array
+// Easy
+
+// 2616. Minimize the Maximum Difference of Pairs
+// Med.
+
+// 2566. Maximum Difference by Remapping a Digit
+// Easy
+
+// 1432. Max Difference You Can Get From Changing an Integer
+// Med.
+
+// 2016. Maximum Difference Between Increasing Elements
+// Easy
+
+// 3405. Count the Number of Arrays with K Matching Adjacent Elements
+// Hard
+
+// 2966. Divide Array Into Arrays With Max Difference
+// Med.
+
+// 2294. Partition Array Such That Maximum Difference Is K
+// Med.
+
+// 3443. Maximum Manhattan Distance After K Changes
+// Med.
+
+// 3085. Minimum Deletions to Make String K-Special
+// Med.
+
+// 2138. Divide a String Into Groups of Size k
+// Easy
+
+// 2081. Sum of k-Mirror Numbers
+// Hard
+
+// 2200. Find All K-Distant Indices in an Array
+// Easy
+
+// 2040. Kth Smallest Product of Two Sorted Arrays
+// Hard
+
+// 2311. Longest Binary Subsequence Less Than or Equal to K
+// Med.
+
+// 2014. Longest Subsequence Repeated k Times
+// Hard
+
+// 2099. Find Subsequence of Length K With the Largest Sum
+// Easy
+
+// 1498. Number of Subsequences That Satisfy the Given Sum Condition
+// Med.
+
+// 594. Longest Harmonious Subsequence
+// Easy
+
+// 3330. Find the Original Typed String I
+// Easy
+
+// 3333. Find the Original Typed String II
+// Hard
+
+// 3304. Find the K-th Character in String Game I
+// Easy
+
+// 3307. Find the K-th Character in String Game II
+// Hard
+
+// 1394. Find Lucky Integer in an Array
+// Easy
+
+// 1865. Finding Pairs With a Certain Sum
+// Med.
+
+// 1353. Maximum Number of Events That Can Be Attended
+// Med.
+
+// 1751. Maximum Number of Events That Can Be Attended II
+// Hard
+
+// 3439. Reschedule Meetings for Maximum Free Time I
+// Med.
+
+// 3440. Reschedule Meetings for Maximum Free Time II
+// Med.
+
+// 2402. Meeting Rooms III
+// Hard
+
+// 1900. The Earliest and Latest Rounds Where Players Compete
+// Hard
+
+// 2410. Maximum Matching of Players With Trainers
+// Med.
+
+// 1290. Convert Binary Number in a Linked List to Integer
+// Easy
+
+// 3136. Valid Word
+// Easy
+
+// 3201. Find the Maximum Length of Valid Subsequence I
+// Med.
+
+// 3202. Find the Maximum Length of Valid Subsequence II
+// Med.
+
+// 2163. Minimum Difference in Sums After Removal of Elements
+// Hard
+
+// 1233. Remove Sub-Folders from the Filesystem
+// Med.
+
+// 1948. Delete Duplicate Folders in System
+// Hard
+
+// 1957. Delete Characters to Make Fancy String
+// Easy
+
+// 1695. Maximum Erasure Value
+// Med.
+
+// 1717. Maximum Score From Removing Substrings
+// Med.
+
+// 2322. Minimum Score After Removals on a Tree
+// Hard
+
+// 3487. Maximum Unique Subarray Sum After Deletion
+// Easy
+
+// 3480. Maximize Subarrays After Removing One Conflicting Pair
+// Hard
+
+// 2210. Count Hills and Valleys in an Array
+// Easy
+
+// 2044. Count Number of Maximum Bitwise-OR Subsets
+// Med.
+
+// 2411. Smallest Subarrays With Maximum Bitwise OR
+// Med.
+
+// 2419. Longest Subarray With Maximum Bitwise AND
+// Med.
+
+// 898. Bitwise ORs of Subarrays
+// Med.
+
+// 118. Pascal's Triangle
+// Easy
+
+// 2561. Rearranging Fruits
+// Hard
+
+// No results found for 'i.first'
+// Cursor added: line 11 column 57
+
+class Solution {
+public:
+    long long minCost(vector<int>& basket1, vector<int>& basket2) {
+        map<int,int>freq;
+        for(int i:basket1)freq[i]++;
+        for(int i:basket2)freq[i]--;
+        vector<int>extra1,extra2;
+        int mini=INT_MAX;
+        for(auto &[val,count]:freq){
+            if(count%2!=0)return -1;
+            int diff=abs(count)/2;
+            mini=min(mini,val);
+            if(count>0){
+                extra1.insert(extra1.end(),diff,val);
+            }else{
+                extra2.insert(extra2.end(),diff,val);
+            }
+        }
+        sort(extra1.begin(),extra1.end());
+        sort(extra2.rbegin(),extra2.rend());
+        long long res=0;
+        for(int i=0;i<extra1.size();i++){
+            res+=min((long long)min(extra1[i],extra2[i]),2LL*mini);
+        } 
+        return res;
+    }
+};
