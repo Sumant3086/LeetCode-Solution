dd7b610297bb8425262889988f85873a4e6cdfad
Sumant Yadav <116252911+Sumant3086@users.noreply.github.com>
2025-12-09 21:38:04 +0530

    Time: 95 ms (97.81%), Space: 200.8 MB (97.81%) - LeetHub

--- 3583-count-special-triplets/3583-count-special-triplets.cpp
+++ 3583-count-special-triplets/3583-count-special-triplets.cpp
@@ -1,41 +1,40 @@
+// class Solution {
+// public:
+//     int specialTriplets(vector<int>& nums) {
+//         const int MOD=1e9+7LL; 
+//         unordered_map<int,long long>right;
+//         unordered_map<int,long long>left;
+//         for(int i:nums)right[i]++;
+//         long long res=0;
+//         for(int j:nums){ 
+//             right[j]--;
+//             int tar=j*2;
+//             if(tar<=200000){
+//                 res=(res+left[tar]*right[tar])%MOD;
+//             }
+//             left[j]++;
+//         }
+//         return (int)res;
+//     }
+// };
+
+
 class Solution {
 public:
     int specialTriplets(vector<int>& nums) {
-        const int MOD=1e9+7LL; 
-        unordered_map<int,long long>right;
-        unordered_map<int,long long>left;
+        const int MOD=1e9+7;
+        long long right[200001]={0},left[200001]={0};
         for(int i:nums)right[i]++;
         long long res=0;
-        for(int j:nums){
-            // int x=nums[j];
-            right[j]--;
-            int tar=j*2;
+        for(int j=0;j<(int)nums.size();j++){
+            int x=nums[j];
+            right[x]--;
+            int tar=x*2;
             if(tar<=200000){
                 res=(res+left[tar]*right[tar])%MOD;
             }
-            left[j]++;
+            left[x]++;
         }
         return (int)res;
     }
 };
-
-
-// class Solution {
-// public:
-//     int specialTriplets(vector<int>& nums) {
-//         const int MOD=1e9+7;
-//         static long long right[200001]={0},left[200001]={0};
-//         for(int i:nums)right[i]++;
-//         long long res=0;
-//         for(int j=0;j<nums.size();j++){
-//             int x=nums[j];
-//             right[x]--;
-//             int tar=x*2;
-//             if(tar<=200000){
-//                 res=(res+left[tar]*right[tar])%MOD;
-//             }
-//             left[x]++;
-//         }
-//         return res;
-//     }
-// };
