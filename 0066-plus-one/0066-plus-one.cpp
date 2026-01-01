e3e872a05d04decad4e196d99e5a59c53f2cf55b
Sumant Yadav <116252911+Sumant3086@users.noreply.github.com>
2026-01-01 19:00:40 +0530

    Time: 0 ms (100%), Space: 11.4 MB (92.86%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -1,26 +1,18 @@
 class Solution {
 public:
-    vector<int> plusOne(vector<int>& digits)
-    {
+    vector<int> plusOne(vector<int>& digits){
         int n=digits.size();
-        for (int i=n-1;i>=0;--i)
-        { 
+        for(int i=n-1;i>=0;i--){
             digits[i]++;
-            if (digits[i]==10)
-            {
+            if(digits[i]==10){
                 digits[i]=0;
-            } else
-            {
+            }else{
                 break;
             }
         }
-        if(digits[0]==0)
-        {
-        digits.insert(digits.begin(), 1);
-        
+        if(digits[0]==0){
+            digits.insert(digits.begin(),1);
         }
         return digits;
     }
-
-    
 };
