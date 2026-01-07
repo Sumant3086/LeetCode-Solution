abc2ea954e1092fccd0c51847f8b7e2cbb58406f
Sumant Yadav <116252911+Sumant3086@users.noreply.github.com>
2026-01-08 04:09:37 +0530

    Time: 7 ms (30.21%), Space: 107 MB (90.21%) - LeetHub

--- 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.cpp
+++ 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.cpp
@@ -11,28 +11,66 @@
  */
 class Solution {
 public:
+    vector<long long>levelSum;
+    void dfs(TreeNode* root,int level){
+        if(!root)return;
+        if(level==levelSum.size()){
+            levelSum.push_back(0);
+        }
+        levelSum[level]+=root->val;
+        dfs(root->left,level+1);
+        dfs(root->right,level+1);
+    }
     int maxLevelSum(TreeNode* root) {
-        queue<TreeNode*>q;
-        q.push(root);
-        int level=1;
-        int maxi=INT_MIN;
+        dfs(root,0);
+        int best=INT_MIN;
+        int level=0;
         int res=1;
-        while(!q.empty()){
-            int n=q.size();
-            int sum=0;
-            while(n--){
-                auto curr=q.front();
-                q.pop();
-                sum+=curr->val;
-                if(curr->left)q.push(curr->left);
-                if(curr->right)q.push(curr->right);
+        for(int i=0;i<levelSum.size();i++){
+            if(levelSum[i]>best){
+                best=levelSum[i];
+                res=i+1;
             }
-            if(sum>maxi){
-                maxi=sum;
-                res=level;
-            }
-            level++;
-        }   
-    return res;
+        }
+        return res;
     }
 };
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+// class Solution {
+// public:
+//     int maxLevelSum(TreeNode* root) {
+//         queue<TreeNode*>q;
+//         q.push(root);
+//         int level=1;
+//         int maxi=INT_MIN;
+//         int res=1;
+//         while(!q.empty()){
+//             int n=q.size();
+//             int sum=0;
+//             while(n--){
+//                 auto curr=q.front();
+//                 q.pop();
+//                 sum+=curr->val;
+//                 if(curr->left)q.push(curr->left);
+//                 if(curr->right)q.push(curr->right);
+//             }
+//             if(sum>maxi){
+//                 maxi=sum;
+//                 res=level;
+//             }
+//             level++;
+//         }   
+//     return res;
+//     }
+// };
