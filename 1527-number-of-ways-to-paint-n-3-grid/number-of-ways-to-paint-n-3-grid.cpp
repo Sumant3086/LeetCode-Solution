5c810a7687ddc3bf627ee9450c2186266b894a8c
Sumant Yadav <116252911+Sumant3086@users.noreply.github.com>
2026-01-27 04:26:24 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (60.76%) - LeetSync

--- 1527-number-of-ways-to-paint-n-3-grid/number-of-ways-to-paint-n-3-grid.cpp
+++ 1527-number-of-ways-to-paint-n-3-grid/number-of-ways-to-paint-n-3-grid.cpp
@@ -1,17 +1,32 @@
 class Solution {
 public:
     int numOfWays(int n) {
-        if(n==1)return 12;
-        if(n==2)return 54;
-        long long prv=54;
-        long long prv2=12;
         long long mod=1e9+7;
-        for(int i=3;i<=n;i++){
-            long long curr=(5*prv-2*prv2)%mod;
-            if(curr<0)curr+=mod;
-            prv2=prv;
-            prv=curr;
+        long long abc=6;
+        long long aba=6;
+        for(int i=2;i<=n;i++){
+            long long prv1=(2*abc+2*aba)%mod;
+            long long prv=(2*abc+3*aba)%mod;
+            abc=prv1;
+            aba=prv;
         }
-        return prv;
+        return (int)((abc+aba)%mod);
     }
 };
+// class Solution {
+// public:
+//     int numOfWays(int n) {
+//         if(n==1)return 12;
+//         if(n==2)return 54;
+//         long long prv=54;
+//         long long prv2=12;
+//         long long mod=1e9+7;
+//         for(int i=3;i<=n;i++){
+//             long long curr=(5*prv-2*prv2)%mod;
+//             if(curr<0)curr+=mod;
+//             prv2=prv;
+//             prv=curr;
+//         }
+//         return prv;
+//     }
+// };
