6eb76e71b6012161598b053da2915a8f00f93334
Sumant Yadav <116252911+Sumant3086@users.noreply.github.com>
2026-02-04 01:50:15 +0530

    Time: 0 ms (100.00%) | Memory: 26 MB (58.68%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,39 +1,25 @@
-// class Solution {
-// public:
-//     int trap(vector<int>& height) { 
-//         int n=height.size();
-//         int left=0;
-//         int right=n-1;
-//         int res=0;
-//         int maxLeft=0;
-//         int maxRight=0;
-//         while(left<=right){
-//             if(height[left]<=height[right]){
-//                 if(height[left]>maxLeft)
-//                 maxLeft=height[left];
-//                 else res+=maxLeft-height[left];
-//                 left++;
-//             }else{
-//                 if(height[right]>maxRight)
-//                 maxRight=height[right];
-//                 else res+=maxRight-height[right];
-//                 right--;
-//             }
-//         }
-//         return res;
-//     }
-// };
-
 class Solution {
 public:
-    //total water is trapped into the bars
-    int trap(vector<int>& h) {
-        int l=0,r=h.size()-1,lmax=INT_MIN,rmax=INT_MIN,ans=0;
+    int trap(vector<int>& height) {
+        int l=0,r=height.size()-1;
+        int lmax=0,rmax=0,res=0;
         while(l<r){
-            lmax=max(lmax,h[l]);
-            rmax=max(rmax,h[r]);
-            ans+=(lmax<rmax)?lmax-h[l++]:rmax-h[r--];            
+            if(height[l]<height[r]){
+                if(lmax<=height[l]){
+                    lmax=height[l];
+                }else{
+                    res+=lmax-height[l];
+                }
+                l++;
+            }else{
+                if(rmax<=height[r]){
+                    rmax=height[r];
+                }else{
+                    res+=rmax-height[r];
+                }
+                r--;
+            }
         }
-        return ans;
+        return res;
     }
 };
